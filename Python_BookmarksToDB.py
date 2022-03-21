bf63e8069ccf8b289cd57ef87abc70bc48c7cc76
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2022-03-21 22:09:50 +0800

    create import_to_db module

--- Python_BookmarksToDB.py
+++ Python_BookmarksToDB.py
@@ -1,5 +1,5 @@
 from bookmarks_class import *
-
+from import_to_db import df_to_db
 #Source filepath (in .html) ---> "C:\\DiskC_Data Stars\\mozilla_bookmarks\\latestBookmark.html"
 #Save filepath (in .csv)    ---> "C:\\DiskC_Data Stars\\mozilla_bookmarks\\bookmark_table.csv"
 # To check available names in global space from bookmarks_class ---> print(globals())
@@ -9,18 +9,4 @@ bkmk_1 = bookmark_saver(source_path)
 save_path = input("Please enter filename or filepath to save extracted data into csv file: ")
 bkmk_1.export_to_csv(save_path)
 
-# conn = sqlite3.connect('Bookmarks_list_db')
-# c = conn.cursor()
-
-# c.execute('''CREATE TABLE IF NOT EXISTS Bookmarks(
-#             Title TEXT,
-#             href TEXT,
-#             add_date TEXT,
-#             last_modified TEXT,
-#             icon_uri TEXT,
-#             tags TEXT)''')
-
-# conn.commit()
-
-
-# bkmk_1.df.to_sql('Bookmarks',conn, if_exists = 'replace', index = False)
+df_to_db(bkmk_1.df)
