12a9f08ce92ae94fecb81eb19e74fa82180297c0
DanielHakim98 <daniel.hakim.jamaluddin@gmail.com>
2022-03-21 21:56:54 +0800

    Split python programs into 2 files

--- Python_BookmarksToDB.py
+++ Python_BookmarksToDB.py
@@ -1,61 +1,26 @@
-from bs4 import BeautifulSoup
-import re
-import codecs
-from datetime import datetime
-import pandas as pd
-from IPython.display import display
-import numpy as np
-from urllib.request import Request,urlopen
-from urllib.error import URLError, HTTPError
-import os
-
-
-class bookmark_saver(BeautifulSoup):
-    def __init__(self, fname):
-        if not os.path.isfile(fname):
-            raise FileNotFoundError("File path is not valid")
-        self.file = codecs.open(fname,'r','utf-8').read()
-        self.soup = BeautifulSoup(self.file,'lxml')
-        self.bookmarks_raw=self.soup.findAll("a")
-        self.bookmarks = list(self.bookmarks_raw)
-        self.delect_icon_att()
-        self.df = self.create_df()
-
-    def delect_icon_att(self):
-        for item in self.bookmarks:
-            del item['icon']
-
-    def create_df(self):
-        book_list = []
-        for item in self.bookmarks:
-            book_dict = self.dict_to_record(item)
-            book_list.append(book_dict)
-        starlist_df = pd.DataFrame(data=book_list)
-        if 'last_charset' in starlist_df.columns.tolist():
-            starlist_df=starlist_df.drop(columns='last_charset')
-        return starlist_df
-    
-    def dict_to_record(self,item):
-        record={}
-        record['Title']=str(item.contents[0])
-        record.update(item.attrs)
-        for key in record:
-            if key == 'add_date' or key=='last_modified':
-                ts=int(record[key])
-                record[key]=datetime.utcfromtimestamp(ts).\
-                            strftime('%Y-%m-%d %H:%M:%S')
-        return record    
-    
-    def export_to_csv(self,path):
-        if not path.endswith('.csv'):
-            raise ValueError("File path doesn't end with .csv")
-        self.df.to_csv(path)
+from bookmarks_class import *
 
 #Source filepath (in .html) ---> "C:\\DiskC_Data Stars\\mozilla_bookmarks\\latestBookmark.html"
 #Save filepath (in .csv)    ---> "C:\\DiskC_Data Stars\\mozilla_bookmarks\\bookmark_table.csv"
-
+# To check available names in global space from bookmarks_class ---> print(globals())
 
 source_path = input("Please enter filepath for targeted html file: ")
 bkmk_1 = bookmark_saver(source_path)
 save_path = input("Please enter filename or filepath to save extracted data into csv file: ")
 bkmk_1.export_to_csv(save_path)
+
+# conn = sqlite3.connect('Bookmarks_list_db')
+# c = conn.cursor()
+
+# c.execute('''CREATE TABLE IF NOT EXISTS Bookmarks(
+#             Title TEXT,
+#             href TEXT,
+#             add_date TEXT,
+#             last_modified TEXT,
+#             icon_uri TEXT,
+#             tags TEXT)''')
+
+# conn.commit()
+
+
+# bkmk_1.df.to_sql('Bookmarks',conn, if_exists = 'replace', index = False)
---
+++ bookmarks_class.py
@@ -0,0 +1,47 @@
+from bs4 import BeautifulSoup
+import codecs
+from datetime import datetime
+import pandas as pd
+import os
+
+
+class bookmark_saver(BeautifulSoup):
+    def __init__(self, fname):
+        if not os.path.isfile(fname):
+            raise FileNotFoundError("File path is not valid")
+        self.file = codecs.open(fname,'r','utf-8').read()
+        self.soup = BeautifulSoup(self.file,'lxml')
+        self.bookmarks_raw=self.soup.findAll("a")
+        self.bookmarks = list(self.bookmarks_raw)
+        self.delect_icon_att()
+        self.df = self.create_df()
+
+    def delect_icon_att(self):
+        for item in self.bookmarks:
+            del item['icon']
+
+    def create_df(self):
+        book_list = []
+        for item in self.bookmarks:
+            book_dict = self.dict_to_record(item)
+            book_list.append(book_dict)
+        starlist_df = pd.DataFrame(data=book_list)
+        if 'last_charset' in starlist_df.columns.tolist():
+            starlist_df=starlist_df.drop(columns='last_charset')
+        return starlist_df
+
+    def dict_to_record(self,item):
+        record={}
+        record['Title']=str(item.contents[0])
+        record.update(item.attrs)
+        for key in record:
+            if key == 'add_date' or key=='last_modified':
+                ts=int(record[key])
+                record[key]=datetime.utcfromtimestamp(ts).\
+                            strftime('%Y-%m-%d %H:%M:%S')
+        return record
+
+    def export_to_csv(self,path):
+        if not path.endswith('.csv'):
+            raise ValueError("File path doesn't end with .csv")
+        self.df.to_csv(path)
